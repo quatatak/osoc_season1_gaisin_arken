320f9d76f6ebc1dff5a43b560385cd1f212fce2f
quatatak <arkengajsin@gmail.com>
2024-03-20 16:55:34 +0500

    include comments

--- ex16.c
+++ ex16.c
@@ -37,22 +37,27 @@ void Person_print(struct Person *who)
 	printf("\tHeight: %d\n", who->height);
 	printf("\tWeight: %d\n", who->weight);
 }
+//initialize main
 int main(int argc, char *argv[]){
+	//create a person with a pointer arken, pointing at this person
 	struct Person *arken = Person_create("Arken", 19, 190, 70);
+	//similarly, here
 	struct Person *chris = Person_create("Chris", 53, 189, 80);
        	
-
+	//print the adress of the person
 	printf("Arken is at memory location %p\n", arken);
+	//print the insides of the person
 	Person_print(arken);
-
+	//print the adress
 	printf("Chris is at memory location %p\n", chris);
+	//print insides
 	Person_print(chris);	
-	
+	//change the atttribute using x->y syntax
 	arken->age = 20;
 	chris->weight = 78;
 	Person_print(arken);
 	Person_print(chris);
-
+	//free the memory by destroying objects
 	Person_destroy(arken);
 	Person_destroy(chris);
 	
