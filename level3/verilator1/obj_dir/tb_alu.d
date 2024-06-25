77059fa1ce33e9c259a4eb6c30c988e9069f48c5
quatatak <arkengajsin@gmail.com>
2024-06-25 23:17:37 +0500

    Completed Part2 and Part3 of Verification with verilator

--- level3/verilator1/obj_dir/tb_alu.d
+++
@@ -1,5 +0,0 @@
-tb_alu.o: ../tb_alu.cpp /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h \
- /usr/share/verilator/include/verilated_vcd_c.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilated_trace.h Valu.h Valu___024unit.h
