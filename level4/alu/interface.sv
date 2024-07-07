953a7934ff664515bde67f238c959436d63ca047
quatatak <arkengajsin@gmail.com>
2024-07-07 23:02:58 +0500

    Getting the testbench ready

---
+++ level4/alu/interface.sv
@@ -0,0 +1,9 @@
+interface alu_interface(input logic clk);
+	
+	//input_1
+	logic [15:0] in_a;
+	//input_2
+	logic [15:0] in_b;
+	//output
+	logic [15:0] out;
+endinterface
--- level4/alu/logic.sv
+++ level4/alu/logic.sv
@@ -5,7 +5,7 @@ module logic(input wire[15:0] in_a, input wire[15:0] in_b, input wire[3:0] sel, 
                         4'b0001:lout = ~(in_a | in_b);
                         4'b0010:lout = ~in_a & in_b;
                         4'b0011:lout = (1'b1<1'b1);
-                        4'b0100:lout = in_a & ~in_b;
+                        4'b0100:lout = ~(in_a & in_b);
                         4'b0101:lout = ~in_b;
                         4'b0110:lout = in_a^in_b;
 			4'b0111:lout = (in_a&~in_b);
@@ -14,7 +14,7 @@ module logic(input wire[15:0] in_a, input wire[15:0] in_b, input wire[3:0] sel, 
                         4'b1010: lout = in_b;
                         4'b1011:lout = in_a&in_b;
                         4'b1100:lout = in_a==in_a;
-                        4'b1101:lout = in_a & (~in_b);
+                        4'b1101:lout = in_a | (~in_b);
 			4'b1110:lout = in_a|in_b;
 			4'b1111:lout = in_a	
 		endcase
