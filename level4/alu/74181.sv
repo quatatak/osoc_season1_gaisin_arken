ec5d4ca16f9b9f41ffb182c985dd8432550df78d
quatatak <arkengajsin@gmail.com>
2024-07-16 17:42:12 +0500

    Setting up the UVM environment

--- level4/alu/74181.sv
+++ level4/alu/74181.sv
@@ -1,10 +1,24 @@
-module alu_module(input wire carry_in, input wire[15:0] in_a, input wire[15:0] in_b, input wire[3:0] select, input wire mode, output wire[15:0] alu_out, output wire carry_out, output wire compare);
-	always_comb begin
-		if (mode == 1'b0) begin
-			arithmetic a1(.in_a(in_a), .in_b(in_b), .sel(select), .out(alu_out), .cout(carry_out), .sel(select), .cin(carry_in), .compare(compare));
-		end
-		else if (mode == 1'b1 ) begin
-			logic l1(.in_a(in_a), .in_b(in_b), .out(alu_out), .sel(select));
-		end
-	end
+module alu_module(input wire carry_in,
+    input wire[15:0] in_a,
+    input wire[15:0] in_b,
+    input wire[3:0] select,
+    input wire mode,
+    output wire[15:0] alu_out,
+    output wire carry_out,
+    output wire compare);
+    always_comb begin
+        if (mode == 1'b0) begin
+            arithmetic a1(.in_a(in_a),
+                .in_b(in_b),
+                .sel(select),
+                .out(alu_out),
+                .cout(carry_out),
+                .sel(select),
+                .cin(carry_in),
+                .compare(compare));
+        end
+        else if (mode == 1'b1 ) begin
+            logic l1(.in_a(in_a), .in_b(in_b), .out(alu_out), .sel(select));
+        end
+    end
 endmodule
--- level4/alu/top.sv
+++ level4/alu/top.sv
@@ -1,18 +1,32 @@
 `include "uvm_macros.svh"
 
 import uvm_pkg::*;
+import alu_pkg::*;
 
-module top();
+module top;
 	alu_interface intf();//instantiated interface
 	
-	alu_module uut();//instance of the alu
+	alu_module uut(
+		.in_a(),
+		.in_b(),
+		.select(),
+		.carry_in(),
+		.alu_out(),
+		.carry_out(),
+		.compare(),
+		.dif(intf)
+	);//instance of the alu
 	
-	initial begin
-		//set
-		uvm_config_db #(virtual alu_interface)::set(null, "*", "intf", intf);
-	end
+	initial
+	begin
+		intf.clk = 0;
+		forever #5 intf.clk = ~intf.clk;
+	end	
 
 	initial begin
+		//set
+		uvm_config_db #(virtual alu_interface)::set(null, "*", "alu_interface", intf);
+		uvm_top.finish_on_completion = 1;
 		run_test("alu_test");
 	end
 
