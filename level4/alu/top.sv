3977ab8cd1df716c36afe6e3838a8465814b256a
arken <arkengajsin@gmail.com>
2024-08-01 17:30:12 +0500

    Formatting and renaming changes

--- level4/alu/top.sv
+++ level4/alu/top.sv
@@ -4,30 +4,29 @@ import uvm_pkg::*;
 import alu_pkg::*;
 
 module top;
-	alu_interface intf();//instantiated interface
-	
-	alu_module uut(
-		.in_a(),
-		.in_b(),
-		.select(),
-		.carry_in(),
-		.alu_out(),
-		.carry_out(),
-		.compare(),
-		.dif(intf)
-	);//instance of the alu
-	
-	initial
-	begin
-		intf.clk = 0;
-		forever #5 intf.clk = ~intf.clk;
-	end	
+  alu_interface intf ();  //instantiated interface
 
-	initial begin
-		//set
-		uvm_config_db #(virtual alu_interface)::set(null, "*", "alu_interface", intf);
-		uvm_top.finish_on_completion = 1;
-		run_test("alu_test");
-	end
+  alu_module uut (
+      .in_a(),
+      .in_b(),
+      .select(),
+      .carry_in(),
+      .alu_out(),
+      .carry_out(),
+      .compare(),
+      .dif(intf)
+  );  //instance of the alu
+
+  initial begin
+    intf.clk = 0;
+    forever #5 intf.clk = ~intf.clk;
+  end
+
+  initial begin
+    //set
+    uvm_config_db#(virtual alu_interface)::set(null, "*", "alu_interface", intf);
+    uvm_top.finish_on_completion = 1;
+    run_test("alu_test");
+  end
 
 endmodule
